5b1d393bd214c85aca2075c2c7757ad6739c6f2f
Hector Prieto Moreno <hector.prieto.moreno@gmail.com>
2020-03-20 13:57:04 -0400

    feature: add unit test and integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,12 @@
-test('Fake Test', ()=> {
-    expect(true).toBeTruthy();
+import {add, total} from './App';
+
+//Unit Test
+test('add function', ()=> {
+    const value = add(1,2);
+    expect(value).toEqual(3);
+})
+
+//Integration test
+test('total function', ()=> {
+    expect(total(5,20)).toBe('$25');
 })
