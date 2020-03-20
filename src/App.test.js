64090df3617eff861af911dcd2eb557e4c831d65
Hector Prieto Moreno <hector.prieto.moreno@gmail.com>
2020-03-20 14:07:12 -0400

    feature: add mock

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,20 @@
-import {add, total} from './App';
+import {total} from './App';
 
+
+//Jest Documentation: https://jestjs.io/docs/en/getting-started.html
+
+//Mock
+const add = jest.fn(() => 3);
 //Unit Test
 test('add function', ()=> {
     const value = add(1,2);
     expect(value).toEqual(3);
+    expect(add).toHaveBeenCalledTimes(1);
+    expect(add).toHaveBeenCalledWith(1,2);
 })
 
 //Integration test
 test('total function', ()=> {
     expect(total(5,20)).toBe('$25');
 })
+
